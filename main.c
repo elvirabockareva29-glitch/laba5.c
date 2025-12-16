954a94815fdf7d724aaa3af773c5b4439c9ad78f
Elya Bochkareva <elvirabockareva29@gmail.com>
2025-12-16 23:15:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,49 @@
-//главная программа: создает массив пациентов- initArray, загружает данные из файла-loadFromFile, показывает меню, запуск нужных функций
-#include <stdio.h>
 #include "hospital.h"
-
-#define FILE_NAME "hospital.dat"
 
 int main() {
     SmartArray arr;
-    initArray(&arr);
-    loadFromFile(&arr, FILE_NAME);
-
+    char filename[100];
     int choice;
 
+    printf("Введите имя файла базы данных: ");
+    scanf("%s", filename);
+
+    initArray(&arr);
+    loadFromFile(&arr, filename);
+
     while (1) {
-        printf("\n===== МЕНЮ =====\n");
-        printf("1. Показать всех пациентов\n");
+        printf("\nМеню:\n");
+        printf("1. Показать пациентов\n");
         printf("2. Добавить пациента\n");
         printf("3. Удалить пациента\n");
         printf("4. Редактировать пациента\n");
         printf("5. Поиск\n");
         printf("0. Выход\n");
-        printf("Выберите: ");
+        printf("Введите номер: ");
+
         scanf("%d", &choice);
 
         switch (choice) {
-        case 1: printPatients(&arr); break;
-        case 2: addPatient(&arr, FILE_NAME); break;
-        case 3: deletePatient(&arr, FILE_NAME); break;
-        case 4: editPatient(&arr, FILE_NAME); break;
-        case 5: searchPatients(&arr); break;
-        case 0: 
-            saveToFile(&arr, FILE_NAME);
-            printf("Выход...\n");
-            return 0;
-        default: printf("Ошибка ввода.\n");
+            case 1:
+                printPatients(&arr);
+                break;
+            case 2:
+                addPatient(&arr, filename);
+                break;
+            case 3:
+                deletePatient(&arr, filename);
+                break;
+            case 4:
+                editPatient(&arr, filename);
+                break;
+            case 5:
+                searchPatients(&arr);
+                break;
+            case 0:
+                free(arr.data);
+                return 0;
+            default:
+                printf("Неверный ввод\n");
         }
     }
 }
